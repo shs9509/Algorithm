76edeeba25fadc784f9b1d693abfc0835a9db114
shs950930@gmail.com <shs950930@gmail.com>
2021-02-10 02:34:35 +0900

    0210

--- 전국대학생프로그래밍대회동아리연합.py
+++ 전국대학생프로그래밍대회동아리연합.py
@@ -2,25 +2,28 @@ a=input()
 a=a.split()
 number=int(a[0])
 yeesan=int(a[1])
-building=int(a[3])
-weeks=int(a[4])
+building=int(a[2])
+weeks=int(a[3])
 cost=list()
-week_number=list()
+num_list=list()
 can_list=list()
 for i in range(building):
     c=input()
     cost.append(c)
     d=input()
     d=d.split()
-    d=list()
-    week_number.append(d)
+    num_list.append(d)
 
-for i,value in enumerate week_number:
-    for j in week_number[i]:
-        if j>number:
-            can_list.append(number*int(cost[i]))
+for p in range(building):
+    for k in num_list[p]:
+        # print(k,number)
+        if int(k) > number:
+            can_list.append(number*int(cost[p]))
 
+# print(can_list)
 if(len(can_list)==0):
+    print("stay home")
+elif(min(can_list)>yeesan):
     print("stay home")
 else:
     print(min(can_list))
---
+++ 좋은구간.py
@@ -0,0 +1,49 @@
+# n= input()
+# n=int(n)
+# m=list(map(int,input().split()))
+# m.sort()
+# o=input()
+# o=int(o)
+# count=0
+# count_li=list()
+
+# if n==1:
+#     count += m[0]-2
+# for i in range(n-1):
+#     if o>m[i] and o<m[i+1]:
+#         for j in range(o-m[i]):
+#             count += m[i+1]-o
+#         count_li.append(count)
+#         count=0
+# if(len(count_li)!=0):
+#     a = max(count_li)
+#     a -= 1
+#     print(a)
+# else:
+#     print(count)
+
+import sys
+input=sys.stdin.readline
+
+l=int(input())
+a=list(map(int,input().split()))
+a=[0]+a
+a.sort()
+n=int(input())
+
+left=0
+right=0
+if n in a:
+    print(0)
+    sys.exit()
+
+for x in a:
+    if n>x:
+        left=x+1
+    else:
+        right=x-1
+        break
+print(left,right)
+print(right-left+(right-n)*(n-left))
+
+
