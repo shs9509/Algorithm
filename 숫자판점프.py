70b7dfc9a8592f2177cb4144de611a21e4400184
shs950930@gmail.com <shs950930@gmail.com>
2021-04-08 21:22:54 +0900

    0408

--- 숫자판점프.py
+++ 숫자판점프.py
@@ -6,30 +6,30 @@
 
 '''
 
-def dfs(x,y,number):
-    if len(number)==6:
-        if number not in tmp:
-            tmp.append(number)
+def dfs(x,y,number):# 시작지점과 경로(number)를 인자로 받는다.
+    if len(number)==6: # 만약 number가 6자리면
+        tmp.append(number)  # tmp에 넣어준다.
         return
 
-    for i in range(4):
+    for i in range(4): # 사방탐색해서 경로를만든다.
         nx = x + dr[i]
         ny = y + dc[i]
-        if 0<=nx<5 and 0<=ny<5:
-            dfs(nx,ny,number+li[nx][ny])
+        if 0<=nx<5 and 0<=ny<5: # 숫자판을 넘지않는다면 dfs를 돌려서 다음경로를 찾는다.
+            dfs(nx,ny,number+li[nx][ny]) # li[nx][ny]를 다음경로로 추가해준다.
+                                         # 이과정을 계속하면 number에 경로가 쌓인다. 
 
-li= list()
-store = list()
-dr = [0,0,-1,1]
+li= list() # 숫자판
+dr = [0,0,-1,1] # 사방탐색을 위한 재료
 dc = [1,-1,0,0]
-tmp = list()
+tmp = list()    # 6번의 경로들이 저장되는 곳
 for i in range(5):
-    li.append(list(map(str, input().split())))
+    li.append(input().split())
 
 
 for x in range(5):
-    for y in range(5):
+    for y in range(5): # 시작지점 x,y를 지정해서 dfs 를 돌린다.
         dfs(x,y,li[x][y])
 
-print(len(tmp))
+
+print(len(set(tmp))) # 중복된경로를 없애기 위해서 set을 써준다.
 
--- 촌수계산.py
+++ 촌수계산.py
@@ -2,35 +2,35 @@
 '''
 이거 걍 bfs 그래프문제 아닌가?
 '''
-def bfs(start,end):
-    visit = [0 for _ in range(num+1)]
-    S=list()
-    S.append(start)
-    visit[start] =1
-    count = 0
+def bfs(start,end): # 인자는 시작노드와 끝노드
+    visit = [0 for _ in range(num+1)] # 방문과 거리표현을 위한 리스트
+    S=list()    # 스택
+    S.append(start) # 시작노드를 스택에 넣고
+    visit[start] =1 # 거리는 1로 표시하면서 방문표시
+    count = 0   # 거리
     while S:
         start = S.pop(0)
-        count = visit[start]### 이거떄매 시간을 얼매네 난린거여
-        count += 1 
-        for j in save[start]:
-            if j == end:
+        count = visit[start] # 시작노드의 거리값을 받는다.
+        count += 1 # 다음노드의 거리는 count +1
+        for j in save[start]:   # 시작노드가 갖고있는 일촌들을 꺼낸다.
+            if j == end:    # 꺼낸게 끝노드면 마무리
                 visit[end] = count-1
                 return visit[end]
-            if visit[j]== 0:
-                visit[j] = count
-                S.append(j)       
-           
-    return -1
+            if visit[j]== 0:    # 꺼낸게 방문안한 노드이면
+                visit[j] = count # 노드에 거리 새겨 주고
+                S.append(j) # 스택에 넣는다.
+    return -1   # 못찾았다는 것은 관계없는 사람이란것이다. -1 출력
 
-num = int(input())
-a, b = map(int, input().split())
-E = int(input())
-save = [[] for _ in range(num+1)]
-for i in range(E):
+
+
+num = int(input()) # 명수
+a, b = map(int, input().split()) # 시작노드, 끝노드
+E = int(input())    # 간선수
+save = [[] for _ in range(num+1)] # 촌수저장 리스트
+for i in range(E): # 간선수만큼 for문 돌려서 일촌상황을 저장한다.
     c,d = map(int,input().split())
     save[c].append(d)
-    save[d].append(c)
-# print(save)
+    save[d].append(c) # 촌수는 양방향 관계!
 print(bfs(a,b))
 
 
