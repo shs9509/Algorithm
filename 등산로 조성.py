6da1cf74e78c197c43013d04c7b13430d1203973
shs950930@gmail.com <shs950930@gmail.com>
2021-04-28 21:19:25 +0900

    0428

--- 등산로 조성.py
+++ 등산로 조성.py
@@ -5,23 +5,74 @@
 기억이안남
 근데 벽부수기 문제를 응용하면 되지않을까?
 '''
-tc = int(input())
-for tc_num in range(tc):
-    scale, K = map(int,input().split())
-    li = list()
-    for i in range(scale):
-        li.append(list(map(int,input().split())))
+# tc = int(input())
+# for tc_num in range(tc):
+#     scale, K = map(int,input().split())
+#     li = list()
+#     for i in range(scale):
+#         li.append(list(map(int,input().split())))
 
-    start_value = 0
-    start_list = list()
-    for x in range(scale):
-        for y in range(scale):
-            if li[x][y]> start_value:
-                start_list.clear()
-                start_list.append((x,y))
-                start_value = li[x][y]
-            elif li[x][y] == start_value:
-                start_list.append((x,y))
-    print(start_list)
+#     start_value = 0
+#     start_list = list()
+#     for x in range(scale):
+#         for y in range(scale):
+#             if li[x][y]> start_value:
+#                 start_list.clear()
+#                 start_list.append((x,y))
+#                 start_value = li[x][y]
+#             elif li[x][y] == start_value:
+#                 start_list.append((x,y))
+#     print(start_list)
 
 
+# 등산로가 진행 가능한지 확인하는 함수
+def wall(x, y, d):
+    if x+dx[d] >= 0 and y+dy[d] >= 0 and x+dx[d] < N and y+dy[d] < N and arr[y][x] > arr[y+dy[d]][x+dx[d]]:
+        return True
+    return False
+ 
+# 등산로 최대 길이를 측정하는 함수
+def path_cnt(x, y, cnt):
+    global max_len_cnt
+    if cnt > max_len_cnt:
+        max_len_cnt = cnt
+    # 4개 방향으로 진행가능한지 확인
+    for d in range(4):
+        if wall(x, y, d):
+            path_cnt(x+dx[d], y+dy[d], cnt+1)
+ 
+# 우 좌 하 상
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+ 
+T = int(input())
+for t in range(1, T+1):
+    N, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+ 
+    max_len_cnt = 0
+ 
+    # 현재 등산로에서 최대 높이, 위치를 측정하는 반복문
+    temp_max_h = 0
+    max_h = []
+    for j in range(N):
+        for i in range(N):
+            if arr[j][i] > temp_max_h:
+                max_h.clear()
+                max_h.append([i,j])
+                temp_max_h = arr[j][i]
+            elif arr[j][i] == temp_max_h:
+                max_h.append([i,j])
+
+    # 최대 높이 위치의 개수만큼 반복 실행
+    while len(max_h):
+        start_x, start_y = max_h.pop()
+        for j in range(N):
+            for i in range(N):
+                # 최대 공사 가능 깊이까지 값을 바꾸며 진행
+                for k in range(1, K+1):
+                    arr[j][i] -= k
+                    path_cnt(start_x, start_y, 1)
+                    arr[j][i] += k
+ 
+    print('#{} {}'.format(t, max_len_cnt))
--- 비숍.py
+++ 비숍.py
@@ -1,1 +1,67 @@
 #https://www.acmicpc.net/problem/1799
+
+def check(idx):
+    c = idx%2 #홀수패턴인지 짝수패턴인지 판별
+    i, j = idx//n, idx%n
+    
+    for d in range(4):
+        x, y = i+dx[d], j+dy[d] 
+        while 0<=x<n and 0<=y<n: 
+            if visited[x*n + y]: # 방문한적있으면 취소
+                return False 
+            x += dx[d] 
+            y += dy[d]  # 대각선 끝까지 탐색
+    return True 
+
+
+def dfs(idx, c, cnt): #좌표, 짝홀패턴, 개수
+    if n*n-idx+1+cnt <= ans[c] or idx >= n*n: # 순회돈 나머지가 ans값보다 작거나, idx가 범위넘어가면
+        return 
+    ans[c] = max(ans[c], cnt) 
+    x, y = idx//n, idx%n  # x,y좌표
+    j = y 
+    for i in range(x, n):
+        while j < n: 
+            v = i*n + j # 1차원배열일때의 좌표
+            if not visited[v] and chess[i][j] == 1 and check(v): 
+                # 방문안하고, 1인공간, 범위가 안넘어감
+                visited[v] = True 
+                dfs(v, c, cnt+1) # 방문체크하고 개수늘림()
+                visited[v] = False 
+            j += 2 
+            #짝수칸씩 이동해야됨
+        j = (c+1)%2 if i%2 == 0 else c #세로줄마다 j의 시작점이 달라야함
+
+'''
+n = 3
+
+흑 백 흑
+
+백 흑 백
+
+흑 백 흑
+
+-> [흑, 백, 흑, 백, 흑, 백, 흑, 백, 흑]
+
+ 
+n = 2
+
+흑 백
+
+흑 백
+
+-> [흑, 백, 흑, 백]
+
+'''
+
+n = int(input()) 
+chess = [list(map(int, input().split())) for _ in range(n)] 
+dx = [1, -1, 1, -1]
+dy = [1, 1, -1, -1] 
+visited = [False] * (n**2) 
+print(visited)
+ans = [0, 0] 
+ # 짝수는 0, 홀수는 1 체스판에서 비숍이 움직일수있는 제한은 2가지 
+dfs(0, 0, 0) 
+dfs(1, 1, 0) 
+print(sum(ans))
