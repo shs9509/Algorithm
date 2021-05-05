49874494a40839e0ef81d81e1e0fc199dac42927
shs950930@gmail.com <shs950930@gmail.com>
2021-05-06 03:15:51 +0900

    0506

--- 수영장.py
+++ 수영장.py
@@ -1,1 +1,30 @@
 #https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PpFQaAQMDFAUq
+
+tc = int(input())
+for tc_num in range(tc):
+    price = list(map(int,input().split())) # 1일 1달 3달 1년
+    schedule = list(map(int,input().split()))
+    schedule.insert(0,0)
+    # value = [0]*13
+    # count = 0
+    # for i in range(1,13):
+    #     if schedule[i]:
+    #         count +=1 
+    #         if schedule[i]*price[0]>= price[1]:
+    #             value[i] = value[i-1] + price[1]
+    #         else:
+    #             value[i] = value[i-1] + schedule[i]*price[0]
+    #     else:
+    #         count = 0
+    #         value[i] = value[i-1]
+        
+    #     if count >= 3:
+    #         if value[i]-value[i-2] >= price[2]:
+    #             value[i] = value[i-2] + price[2]
+        
+    #     if i ==12:
+    #         if value[12]>=price[3]:
+    #             value[12]==price[3]
+
+    # print(value[12])
+    
--- 퇴사.py
+++ 퇴사.py
@@ -1,1 +1,21 @@
 #https://www.acmicpc.net/problem/14501
+
+def maximum_earning(day,earning):
+    for j in range(day,date+1):
+        if j+li[j][0]-1<date+1:
+            earn.append(earning+li[j][1])
+            maximum_earning(j+li[j][0],earning+li[j][1])          
+
+date = int(input())
+li = list()
+li.append(0)
+earn = list()
+earn.append(0)
+for i in range(date):
+    li.append(list(map(int,input().split())))
+
+maximum_earning(1,0)
+print(max(earn))
+
+
+# 91%에서 멈추는데 그것은 0일때를 고려안해서
