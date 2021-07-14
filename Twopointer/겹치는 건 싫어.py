08eb7711626b2d4f95cbae660da5a57487044a7a
shs950930@gmail.com <shs950930@gmail.com>
2021-07-15 01:42:05 +0900

    0714

--- Twopointer/겹치는 건 싫어.py
+++ Twopointer/겹치는 건 싫어.py
@@ -1,20 +1,46 @@
 #0627 겹치는 건 싫어 : https://www.acmicpc.net/problem/20922
 
-length, n = map(int, input().split())
-li = list(map(int,input().split()))
-save = [0 for _ in range(length+1)]
-save_li= list()
+# length, n = map(int, input().split())
+# li = list(map(int,input().split()))
+# save = [0 for _ in range(length+1)]
+# save_li= list()
 
-count = 0
-for i in range(length):
-    # print(save)
-    save[li[i]] +=1
-    count +=1
-    if save[li[i]]>n:
-        save = [ 0 for _ in range(length+1)]
-        save[li[i]] +=1
-        save_li.append(count-1)
-        count = 0
-save_li.append(count)
-# print(save_li)
-print(max(save_li))
+# count = 0
+# for i in range(length):
+#     # print(save)
+#     save[li[i]] +=1
+#     count +=1
+#     if save[li[i]]>n:
+#         save = [ 0 for _ in range(length+1)]
+#         save[li[i]] +=1
+#         save_li.append(count-1)
+#         count = 0
+# save_li.append(count)
+# # print(save_li)
+# print(max(save_li))
+
+###################################
+
+from sys import stdin
+from collections import deque
+
+n, k = map(int, input().split())
+arr = list(map(int, stdin.readline().split()))
+
+def answer():
+  global n, k, arr
+  startIndex, maxCount = (0, 0)
+  cnt = [0]*100001
+  for i in range(len(arr)):
+    num = arr[i]
+    cnt[num] = cnt[num] + 1
+    if cnt[num] > k:
+      maxCount = max(maxCount, i-startIndex)
+      while(cnt[num] > k):
+        cnt[arr[startIndex]] = cnt[arr[startIndex]] - 1
+        startIndex += 1
+    else:
+      maxCount = max(maxCount, i-startIndex+1)
+  return maxCount
+  
+print(answer())
---
+++ Twopointer/두 용액 .py
@@ -0,0 +1,58 @@
+#0715 두 용액 : https://www.acmicpc.net/problem/2470
+
+n = int(input())
+li = list(map(int, input().split()))
+li = sorted(li)
+# print(li)
+end = n-1
+start = 0
+best_start =0
+best_end =0
+zin=4000000000
+while start<end:
+    mix = li[start]+li[end] # 이게 문제 였음
+    if abs(zin) >= abs(li[start]+li[end]):
+        zin = li[start]+li[end]
+        best_start = start
+        best_end = end
+        if zin ==0:
+            break
+    if mix <= 0: # 여기도 zin 이였음
+        start +=1
+    else:
+        end -=1
+print(li[best_start],li[best_end])
+    # else:
+    #     print(li[past_start],li[past_end])
+    #     break
+
+# 5
+# -100 -50 20 40 80
+
+# import sys
+
+# n = int(sys.stdin.readline())
+# liquid = sorted(list(map(int, sys.stdin.readline().split())))
+
+# pl = 0
+# pr = len(liquid) - 1
+# limit = liquid[pr] + liquid[0]
+# small = 0
+# large = 0
+
+# while pl < pr:
+#     mix = liquid[pl] + liquid[pr]
+
+#     if abs(mix) <= abs(limit):
+#         limit = mix
+#         small = pl
+#         large = pr
+#         if limit == 0:
+#             break
+
+#     if mix >= 0:
+#         pr -= 1
+#     else:
+#         pl += 1
+
+# print(liquid[small], liquid[large])
