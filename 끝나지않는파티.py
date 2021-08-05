83de839a80ba82970661bc8b0f912f04c6ff7555
신형식 <shs950930@gmail.com>
2021-08-05 22:13:44 +0900

    solve baekjoon 11265

--- 끝나지않는파티.py
+++ 끝나지않는파티.py
@@ -5,52 +5,53 @@ li= list()
 for i in range(n):
     li.append(list(map(int,input().split())))
 
-def floid(lis):
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                lis[j][k] = min(lis[j][k], lis[j][i]+lis[i][k] )
-
-floid(li)
-for time in range(times):
-    start, end, t = list(map(int,input().split()))
-    if li[start-1][end-1]<=t:
-        print('Enjoy other party')
-    else:
-        print('Stay here')
-
-
-# def find_route(S,E,T,V,C):
-#     if V>T:
-#         return
-#     if S==E:
-#         if T>=V:
-#             possible = True
-#         return
-#     else:
+# def floid(lis):
+#     for i in range(n):
 #         for j in range(n):
-#             if C[j]:
-#                 continue
-#             else:
-#                 C[j] = True
-#                 find_route(j,E,T,V+li[S][j],C)
-#                 C[j] = False
-#         else:
-#             return
+#             for k in range(n):
+#                 lis[j][k] = min(lis[j][k], lis[j][i]+lis[i][k] )
 
-
+# floid(li)
 # for time in range(times):
-#     check = [False for _ in range(n)]
 #     start, end, t = list(map(int,input().split()))
-#     possible = False
-#     if li[start-1][end-1] <= t:
-#         print('Enjoy other party')
-#         continue
-#     else:
-#         check[start-1] = True
-#         find_route(start-1,end-1,t,0,check)
-
-#     if possible:
+#     if li[start-1][end-1]<=t:
 #         print('Enjoy other party')
 #     else:
 #         print('Stay here')
+
+
+def find_route(S,E,T,V,C):
+    global possible
+    if V>T:
+        return
+    if S==E:
+        if T>=V:
+            possible = True
+            return True
+    else:
+        for j in range(n):
+            if C[j]:
+                continue
+            else:
+                C[j] = True
+                a = find_route(j,E,T,V+li[S][j],C)
+                C[j] = False
+                if a: 
+                    return True
+
+
+for time in range(times):
+    check = [False for _ in range(n)]
+    start, end, t = list(map(int,input().split()))
+    possible = False
+    if li[start-1][end-1] <= t:
+        print('Enjoy other party')
+        continue
+    else:
+        check[start-1] = True
+        find_route(start-1,end-1,t,0,check)
+
+    if possible:
+        print('Enjoy other party')
+    else:
+        print('Stay here')
