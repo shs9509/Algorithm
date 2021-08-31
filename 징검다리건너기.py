5b6bcb1918f0ee5c9ef8493110a3ccfc5766d12a
Hyungsik <shs950930@gmail.com>
2021-08-31 23:44:14 +0900

    백준 문제풀이

--- 징검다리건너기.py
+++ 징검다리건너기.py
@@ -44,3 +44,21 @@ def dfs( pos, big_jump, val):
 
 dfs(1,True,0)
 print(min(lis))
+
+#########################################
+# DP 로 푸는 방법##########################
+#########################################
+dp = [[200000, 200000] for _ in range(24)]
+dp[0][1] = 0
+n = int(input())
+energys = []
+for _ in range(n - 1):
+    s, b = input().split()
+    energys.append((int(s), int(b)))
+k = int(input())
+for i in range(n-1):
+    for very_big in range(2):
+        dp[i + 1][very_big] = min(dp[i + 1][very_big], dp[i][very_big] + energys[i][0])
+        dp[i + 2][very_big] = min(dp[i + 2][very_big], dp[i][very_big] + energys[i][1])
+    dp[i + 3][0] = min(dp[i + 3][0], dp[i][1] + k)
+print(min(dp[n-1][1], dp[n-1][0]))
