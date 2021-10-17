4b0be13f23a596e34cd3661403879eae8cff2bea
Hyungsik <shs950930@gmail.com>
2021-10-17 21:23:49 +0900

    Solve : baekjoon 11047

---
+++ 동전0.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/11047
+n, k = map(int, input().split())
+m = []
+num = 0
+for i in range(n):
+    m.append(int(input()))
+for i in range(n - 1, -1, -1):
+    if k == 0:
+        break
+    if m[i] > k:
+        continue
+    num += k // m[i]
+    k %= m[i]
+print(num)
