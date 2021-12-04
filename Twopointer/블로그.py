bc2d9061a1f2fff17aecf477c83ea26978318958
Hyungsik <shs950930@gmail.com>
2021-12-04 23:12:03 +0900

    Solve : baekjoon 21921

---
+++ Twopointer/블로그.py
@@ -0,0 +1,28 @@
+#https://www.acmicpc.net/problem/21921
+
+n, x = map(int, input().split())
+xList = list(map(int, input().split()))
+
+s = sum(xList[:x])
+l = 0
+r = x - 1
+M = 0
+cnt = 0
+while True:
+    if M < s:
+        cnt = 1
+        M = s
+    elif M == s:
+        cnt += 1
+    r += 1
+    if r == n:
+        break
+    s += xList[r]
+    s -= xList[l]
+    l += 1
+
+if M == 0:
+    print('SAD')
+else:
+    print(M)
+    print(cnt)
