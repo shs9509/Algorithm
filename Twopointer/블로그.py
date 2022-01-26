6f16255ccccd02c93a25ff293b18ce9d7ab09141
신형식 <shs950930@gmail.com>
2022-01-26 20:47:36 +0900

    Solve: Baekjoon 21921

--- Twopointer/블로그.py
+++ Twopointer/블로그.py
@@ -1,28 +1,51 @@
 #https://www.acmicpc.net/problem/21921
 
-n, x = map(int, input().split())
-xList = list(map(int, input().split()))
+# n, x = map(int, input().split())
+# xList = list(map(int, input().split()))
 
-s = sum(xList[:x])
-l = 0
-r = x - 1
-M = 0
-cnt = 0
-while True:
-    if M < s:
-        cnt = 1
-        M = s
-    elif M == s:
-        cnt += 1
-    r += 1
-    if r == n:
-        break
-    s += xList[r]
-    s -= xList[l]
-    l += 1
+# s = sum(xList[:x])
+# l = 0
+# r = x - 1
+# M = 0
+# cnt = 0
+# while True:
+#     if M < s:
+#         cnt = 1
+#         M = s
+#     elif M == s:
+#         cnt += 1
+#     r += 1
+#     if r == n:
+#         break
+#     s += xList[r]
+#     s -= xList[l]
+#     l += 1
 
-if M == 0:
+# if M == 0:
+#     print('SAD')
+# else:
+#     print(M)
+#     print(cnt)
+
+full_length, length = map(int,input().split())
+li = list(map(int,input().split()))
+
+# start = sum(li[0:length])
+start = sum(li[:length])
+max_val = start
+count =1
+for i in range(length,full_length):
+    start = start - li[i-length] + li[i]
+    if max_val<start:
+        max_val = start
+        count=1
+    elif max_val == start:
+        count+=1
+    else:
+        continue
+
+if max_val==0:
     print('SAD')
 else:
-    print(M)
-    print(cnt)
+    print(max_val)
+    print(count)
