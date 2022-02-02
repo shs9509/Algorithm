24bc8e074d4b538c02e2fd873899bcf8a350caaf
신형식 <shs950930@gmail.com>
2022-02-02 23:04:54 +0900

    Solve: Baekjoon 1806

---
+++ Twopointer/부분합.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/1806
+
+num, obj = map(int,input().split())
+li = list(map(int,input().split()))
+s=0
+e=1
+sum_val = li[s]
+min_length =1000000
+while(s<=e):
+    if sum_val>=obj:
+        min_length = min(e-s,min_length)
+        sum_val-=li[s]
+        s+=1
+        # print(sum(li[s:e]))
+        if s>=num:
+            break
+    else:
+        if e>=num:
+            if sum_val<obj: #e가끝까지 도착하고나서 이미 목표치를 도달못하면
+                break
+            else:
+                continue
+        else:
+            sum_val+=li[e]
+            e+=1
+
+    # print(s,e,sum_val)
+if min_length == 1000000:
+    print('0')
+else:
+    print(min_length)
+
