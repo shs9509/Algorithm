3460159bcabc73f97c9a3bcfa52b99f909f53509
신형식 <shs950930@gmail.com>
2022-02-21 23:28:57 +0900

    Solve: Baekjoon 9655

--- DP/돌게임.py
+++ DP/돌게임.py
@@ -1,12 +1,28 @@
 #https://www.acmicpc.net/problem/9655
-n=int(input())
-li= [0,1,0,1]
+# n=int(input())
+# li= [0,1,0,1]
+
+# for i in range(4,n+1):
+#     if li[i-1]==0 and li[i-3]==0:
+#         li.append(1)
+#     elif li[i-1]==1 and li[i-3]==1:
+#         li.append(0)
+# if li[n]==1:
+#     print('SK')
+# else:
+#     print('CY')
+
+
+n = int(input())
+
+li =[0,1,0,1]
 
 for i in range(4,n+1):
-    if li[i-1]==0 and li[i-3]==0:
-        li.append(1)
-    elif li[i-1]==1 and li[i-3]==1:
+    if li[i-1]==1 and li[i-3]==1:
         li.append(0)
+    elif li[i-1]==0 and li[i-3]==0:
+        li.append(1)
+
 if li[n]==1:
     print('SK')
 else:
