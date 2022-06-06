c51d62785a4d66a2c721777fde99fd2258d5c18d
shs9509 <shs950930@gmail.com>
2022-06-06 23:34:23 +0900

    Solve: Programmers/Level3/[Python]디스크컨트롤러

---
+++ 프로그래머스/Level3/디스크컨트롤러.py
@@ -0,0 +1,40 @@
+# import itertools
+
+# def solution(jobs):
+#     orders = [a for a in range(len(jobs))]
+#     disc_orders = list(itertools.permutations(orders, len(jobs)))
+#     # print(disc_orders)
+#     answer = []
+#     for disc_order in disc_orders:
+#         count=0
+#         last=jobs[disc_order[0]][0]
+#         for order in disc_order:
+#             count+= jobs[order][1] + abs(jobs[order][0]-last)
+#             last+= jobs[order][1]
+#             # print(count)
+#         answer.append(count//len(jobs))
+#     return min(answer)
+
+import heapq
+
+def solution(jobs):
+    answer, now, i = 0, 0, 0
+    start = -1 
+    heap = []
+    
+    while i < len(jobs):
+        # 현재 시점에서 처리할 수 있는 작업을 heap에 저장
+        for j in jobs:
+            if start < j[0] <= now:
+                heapq.heappush(heap, [j[1], j[0]])
+        
+        if len(heap) > 0: # 처리할 작업이 있는 경우
+            cur = heapq.heappop(heap)
+            start = now
+            now += cur[0]
+            answer += now - cur[1] # 작업 요청시간부터 종료시간까지의 시간 계산
+            i +=1
+        else: # 처리할 작업이 없는 경우 다음 시간을 넘어감
+            now += 1
+                
+    return answer // len(jobs)
